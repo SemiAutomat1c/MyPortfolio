1d4bfcc35f304fb6b63ab7abb475cfc85bbeacb0
Ryan Deniega <103834718+SemiAutomat1c@users.noreply.github.com>
2025-07-02 20:42:26 +0800

    fix: add use client directive and optimize build config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const withMDX = require('@next/mdx')();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  staticPageGenerationTimeout: 180,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+  images: {
+    unoptimized: true
+  }
 };
 
 module.exports = withMDX(nextConfig); 
